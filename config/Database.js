6507a76756870b106b8e73b0acd0134b712aae22
amin <muhammadalamin908@gmail.com>
2025-06-13 11:24:34 +0700

    Reconfiguration config.database

--- config/Database.js
+++ config/Database.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize("scedule_petani", "root", "", {
-    host: "localhost",
-    port: "3306",
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql", 
 });
 
